178270681386a63bfb7471f1f910a6e2c3861ee4
RobertNeher <Robert.Neher@gmail.com>
2023-07-14 13:33:00 +0200

    minor fix (self)

--- src/helper.py
+++ src/helper.py
@@ -3,5 +3,5 @@ from datetime import datetime
 def extract_list(list_string):
     return list_string.strip("[]").replace("'", "").split(", ")
 
-def formatDate(self, dBYdate):
+def formatDate(dBYdate):
     return datetime.strftime(datetime.strptime(dBYdate, "%d. %B %Y"), "%Y-%m-%d")
